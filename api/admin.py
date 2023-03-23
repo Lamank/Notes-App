c55f77005b705b0f28931b29fded1720c3e4d4d2
Laman Kalbiyeva <lamankalbiyeva@gmail.com>
2023-03-23 17:42:22 +0400

    add some filters

--- api/admin.py
+++ api/admin.py
@@ -4,6 +4,11 @@ from api.models import Note
 
 class NoteModelAdmin(admin.ModelAdmin):
     list_display = ('body', 'created', 'updated',)
+    list_display_links = ('body', )
+    readonly_fields = ("created", "updated",)
+    list_filter = ("created", "body", "updated",)
+    empty_value_display = 'unknown'
+    search_fields = ("body",)
     
 admin.site.register(Note, NoteModelAdmin)
 
